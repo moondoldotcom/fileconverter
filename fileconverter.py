68341a82f7aa4f2d0276033f6865e07f9ec7fae8
moondoldotcom <139631857+moondoldotcom@users.noreply.github.com>
2023-07-20 22:57:20 +0900

    fileconverter.py

--- fileconverter.py
+++ fileconverter.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PIL import Image
-from pdf2docx import Converter
 import base64
 import os
-from pptx import Presentation
+from pdf2image import convert_from_path
+from docx2pdf import convert
 
 def convert_image(file, format):
     image = Image.open(file)
@@ -13,21 +13,21 @@ def convert_image(file, format):
     image.save(new_filename, format)
     return new_filename
 
-def convert_pdf_to_word(file):
-    pdf_file = file.name
-    word_file = pdf_file.split(".")[0] + ".docx"
-    cv = Converter(pdf_file)
-    cv.convert(word_file, start=0, end=None)
-    cv.close()
-    return word_file
+def convert_pdf_to_images(file, format):
+    images = convert_from_path(file.name)
+    output_files = []
+    for i, image in enumerate(images):
+        if image.mode in ("RGBA", "P"): 
+            image = image.convert("RGB")
+        output_file = f"{file.name.split('.')[0]}_{i+1}.{format}"
+        image.save(output_file, format)
+        output_files.append(output_file)
+    return output_files
 
-def convert_jpg_to_pptx(file):
-    prs = Presentation()
-    slide = prs.slides.add_slide(prs.slide_layouts[5])
-    slide.shapes.add_picture(file, 0, 0)
-    new_filename = file.name.split(".")[0] + ".pptx"
-    prs.save(new_filename)
-    return new_filename
+def convert_word_to_pdf(file):
+    output_file = file.name.split(".")[0] + ".pdf"
+    convert(file.name, output_file)
+    return output_file
 
 def create_download_link(file):
     with open(file, "rb") as f:
@@ -36,12 +36,12 @@ def create_download_link(file):
         href = f'<a href="data:file/{file.split(".")[-1]};base64,{b64}" download="{os.path.basename(file)}">Download file</a>'
         return href
 
-st.title('JPG/PNG/PDF/JPEG/DOCX/PPTX Converter(파일 확장자 변환기)')
+st.title('Image to Image / PDF to Image / Word to PDF Converter(파일 변환기)')
 
-file = st.file_uploader("Upload a file", type=['jpg', 'png', 'pdf'])
+file = st.file_uploader("Upload a file", type=['jpg', 'png', 'pdf', 'docx'])
 
 if file is not None:
-    file_type = st.selectbox("Select output format", ['jpg', 'png', 'pdf', 'docx', 'pptx'])
+    file_type = st.selectbox("Select output format", ['jpg', 'png', 'pdf'])
 
     if st.button('Convert'):
         if file_type in ['jpg', 'png']:
@@ -49,26 +49,17 @@ if file is not None:
                 output_file = convert_image(file, file_type)
                 st.success(f'File converted successfully.')
                 st.markdown(create_download_link(output_file), unsafe_allow_html=True)
+            elif file.type == 'application/pdf':
+                output_files = convert_pdf_to_images(file, file_type)
+                for output_file in output_files:
+                    st.success(f'File converted successfully.')
+                    st.markdown(create_download_link(output_file), unsafe_allow_html=True)
             else:
-                st.error('Please upload a jpg or png file to convert to another image format.')
+                st.error('Please upload a jpg, png, or pdf file to convert to another image format.')
         elif file_type == 'pdf':
-            if file.type in ['image/jpeg', 'image/png']:
-                output_file = convert_image(file, file_type)
+            if file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                output_file = convert_word_to_pdf(file)
                 st.success(f'File converted successfully.')
                 st.markdown(create_download_link(output_file), unsafe_allow_html=True)
             else:
-                st.error('Please upload a jpg or png file to convert to pdf.')
-        elif file_type == 'docx':
-            if file.type == 'application/pdf':
-                output_file = convert_pdf_to_word(file)
-                st.success(f'File converted successfully.')
-                st.markdown(create_download_link(output_file), unsafe_allow_html=True)
-            else:
-                st.error('Please upload a pdf file to convert to docx.')
-        elif file_type == 'pptx':
-            if file.type in ['image/jpeg', 'image/png']:
-                output_file = convert_jpg_to_pptx(file)
-                st.success(f'File converted successfully.')
-                st.markdown(create_download_link(output_file), unsafe_allow_html=True)
-            else:
-                st.error('Please upload a jpg or png file to convert to pptx.')
+                st.error('Please upload a docx file to convert to pdf.')
